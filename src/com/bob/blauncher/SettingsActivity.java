227b25106e96b022318f1be891c61e320c9a6d85
Bob Qi <a483334@fmr.com>
2014-02-20 10:01:11 +0800

    use setting activity as a launcher

--- src/com/bob/blauncher/SettingsActivity.java
+++ src/com/bob/blauncher/SettingsActivity.java
@@ -1,11 +1,19 @@
 package com.bob.blauncher;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -15,12 +23,6 @@ import com.bob.blauncher.query.Action;
 import com.bob.blauncher.query.Source;
 import com.bob.blauncher.query.Utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by Bob on 6/12/13.
  */
@@ -29,7 +31,7 @@ public class SettingsActivity extends Activity {
     private static final String FORMAT_NAME_KEY = "%s (%s)";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
@@ -40,6 +42,25 @@ public class SettingsActivity extends Activity {
     public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(this, LauncherActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( final Menu menu )
+    {
+        super.onCreateOptionsMenu(menu);
+        menu.add("Set Wallpaper");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( final MenuItem item )
+    {
+        if ("Set Wallpaper".equals(item.getTitle()))
+        {
+            startActivity(Intent.createChooser(new Intent(Intent.ACTION_SET_WALLPAPER), "Choose wallpaper"));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -59,17 +80,17 @@ public class SettingsActivity extends Activity {
         textColorView.setText(textColor);
         textColorView.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            public void beforeTextChanged(final CharSequence charSequence, final int i, final int i2, final int i3) {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            public void onTextChanged(final CharSequence charSequence, final int i, final int i2, final int i3) {
 
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
+            public void afterTextChanged(final Editable editable) {
                 try {
                     String text = editable.toString();
                     int color = Color.parseColor(text);
@@ -88,7 +109,7 @@ public class SettingsActivity extends Activity {
                 new SimpleAdapter(this, getData(utils.getAllSources()), android.R.layout.simple_list_item_2, new String[]{"source", "action"}, new int[]{android.R.id.text1, android.R.id.text2}));
     }
 
-    private List<Map<String, String>> getData(Collection<Source> sources) {
+    private List<Map<String, String>> getData(final Collection<Source> sources) {
         List<Map<String, String>> data = new ArrayList<Map<String, String>>(sources.size());
         for (Source s : sources) {
             Map<String, String> map = new HashMap<String, String>();
